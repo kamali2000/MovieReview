094a2f86b503b7783104490e9a19413ec0f28a54
Kamali <173637x@mymail.nyp.edu.sg>
2018-11-16 20:54:34 +0800

    Initial commit

---
+++ app/src/main/java/comb/nyp/sit/dit/it2107/moviereview/MainActivity.kt
@@ -0,0 +1,12 @@
+package comb.nyp.sit.dit.it2107.moviereview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
